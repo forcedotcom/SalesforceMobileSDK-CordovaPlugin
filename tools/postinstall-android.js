3655bb3f83270c1f2973d0231b0e895fb08fb0cc
Bharath Hariharan <bhariharan@salesforce.com>
2015-10-20 16:15:39 -0700

    Fixing Gradle plugin version dependency

--- tools/postinstall-android.js
+++ tools/postinstall-android.js
@@ -109,5 +109,8 @@ var oldAndroidDepTree = "android {";
 var newAndroidDepTree = "android {\n\tpackagingOptions {\n\t\texclude 'META-INF/LICENSE'\n\t\texclude 'META-INF/LICENSE.txt'\n\t\texclude 'META-INF/DEPENDENCIES'\n\t\texclude 'META-INF/NOTICE'\n\t}";
 shelljs.sed('-i', oldAndroidDepTree, newAndroidDepTree, path.join(appProjectRoot, 'build.gradle'));
 shelljs.echo("allprojects {\n\trepositories {\n\t\tmavenCentral\(\)\n\t}\n}").toEnd(path.join(appProjectRoot, 'build.gradle'));
+var oldBuildScriptDepTree = "buildscript {";
+var newBuildScriptDepTree = "buildscript {\n\tdependencies {\n\t\tclasspath 'com.android.tools.build:gradle:1.3.1'\n\t}\n";
+shelljs.sed('-i', oldBuildScriptDepTree, newBuildScriptDepTree, path.join(appProjectRoot, 'build.gradle'));
 
 console.log("Done running SalesforceMobileSDK plugin android post-install script");
