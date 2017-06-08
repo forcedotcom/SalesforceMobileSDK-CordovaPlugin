bcdc1742729ae876c290870d3ad9a4c32390636e
Gabe Sumner <gabe.sumner@gmail.com>
2017-06-08 15:39:23 -0700

    Attempting fix of push notification callback.

--- www/com.salesforce.util.push.js
+++ www/com.salesforce.util.push.js
@@ -56,9 +56,7 @@ var registerPushNotificationHandler = function(notificationHandler, fail) {
         push.on('notification', function(data) {
           console.log("notification event");
           console.log(JSON.stringify(data));
-          if (data.event == "message") {
-            notificationHandler(message);
-          }
+          notificationHandler(message);
           push.finish(function () {
               console.log('finish successfully called');
           });
