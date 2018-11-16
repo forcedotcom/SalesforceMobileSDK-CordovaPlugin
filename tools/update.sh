2bffc8d20d5701e6f9d85ca123f30eaefaa4ead4
Wolfgang Mathurin <wmathurin@salesforce.com>
2018-11-15 20:11:42 -0800

    Another fix for update.sh

--- tools/update.sh
+++ tools/update.sh
@@ -74,7 +74,7 @@ parse_opts ()
 copy_and_fix ()
 {
     echo "* Fixing and copying $1 to $2 directory"
-    find tmp -name $1 | xargs sed -E 's/#import <(SalesforceAnalytics|SalesforceSDKCore|SmartStore|SmartSync|SalesforceHybrid|CocoaLumberjack).*\/(.*)>/#import "\2"/' > src/ios/$2/$1
+    find tmp -name $1 | xargs sed -E 's/#import <(SalesforceSDKCommon|SalesforceAnalytics|SalesforceSDKCore|SmartStore|SmartSync|SalesforceHybrid).*\/(.*)>/#import "\2"/' > src/ios/$2/$1
 }
 
 copy_lib ()
