e0fd9a4c1c3e27bf6fba0d6749d19aae1e6fb88a
Wolfgang Mathurin <wmathurin@salesforce.com>
2023-09-27 19:23:27 -0700

    Trying project file tweaks instead

--- tools/postinstall-ios.js
+++ tools/postinstall-ios.js
@@ -3,21 +3,24 @@ console.log('Running SalesforceMobileSDK plugin ios post-install script');
 const fs = require('fs');
 const path = require('path');
 
+function replaceTextInFile(fileName, textInFile, replacementText) {
+    const contents = fs.readFileSync(fileName, 'utf8');
+    const lines = contents.split(/\r*\n/);
+    const result = lines.map(function (line) {
+        return line.replace(textInFile, replacementText);
+    }).join('\n');
+    fs.writeFileSync(fileName, result, 'utf8'); 
+}
+
+
+console.log('Pointing to AppDelegate.m defined in plugin');
 const appProjectRoot = path.join('platforms', 'ios');
-const appName = path.parse(fs.readdirSync(appProjectRoot).filter(f=>f.endsWith('.xcworkspace'))[0]).name
-const pluginAppDelegate = path.join('plugins', 'com.salesforce', 'src', 'ios', 'classes', 'AppDelegate.m');
-const appAppDelegate = path.join(appProjectRoot, appName, 'AppDelegate.m')
-
-console.log('Moving AppDelegate.m to the correct location');
-fs.copyFile(pluginAppDelegate, appAppDelegate,
-	    err => {
-		if (err) {
-		    console.error(`Error copying file: ${err}`)
-		    process.exit(1);
-		}
-	    }
-	   );
-
-fs.rmSync(pluginAppDelegate)
+const appName = path.parse(fs.readdirSync(appProjectRoot).filter(f=>f.endsWith('.xcworkspace'))[0]).name;
+const projectFile = path.join(appProjectRoot, `${appName}.xcodeproj`, 'project.pbxproj');
+//replaceTextInFile(projectFile, 'path = AppDelegate.m;', 'name = AppDelegate.m; path = com.salesforce/AppDelegate.m;');
 
 console.log('Done running SalesforceMobileSDK plugin ios post-install script');
+
+
+
+
