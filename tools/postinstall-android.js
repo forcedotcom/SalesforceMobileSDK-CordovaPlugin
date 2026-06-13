7278569533d372ea0bd167a88478d32155ed926c
Wolfgang Mathurin <wmathurin@salesforce.com>
2026-06-12 20:06:28 -0700

    W-19408803: Only replace package declaration line, not all occurrences

--- tools/postinstall-android.js
+++ tools/postinstall-android.js
@@ -87,7 +87,7 @@ if (packageMatch) {
     const mainAppSrc = path.join(pluginRoot, 'src', 'android', 'MainApplication.kt');
     const mainAppDest = path.join(mainAppSrcDir, 'MainApplication.kt');
     shelljs.cp(mainAppSrc, mainAppDest);
-    replaceTextInFile(mainAppDest, 'com.salesforce.androidsdk.phonegap.app', packageName);
+    replaceTextInFile(mainAppDest, 'package com.salesforce.androidsdk.phonegap.app', `package ${packageName}`);
 
     // Set android:name in AndroidManifest.xml to point to the app's MainApplication.
     // plugin.xml no longer sets android:name, so we inject it here into the <application> tag.
